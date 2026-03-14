66ea8ad4df5f1484f95f8d81116433050c31c714
adautoaraujo414-pixel <adautoaraujo414@gmail.com>
2026-03-14 15:40:02 +0000

    fix: authMiddleware → validarAdmin no admin.routes.js

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { authMiddleware } = require('../middlewares/auth.middleware');
+const { validarAdmin: authMiddleware } = require('../middlewares/auth.middleware');
 const router = express.Router();
 const PrecoSimplesService = require('../services/preco-simples.service');
 
