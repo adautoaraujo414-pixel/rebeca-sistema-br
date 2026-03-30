7d0afa223752b4ea316f9eecb6b4a2bdf1c5d208
adautoaraujo414-pixel <adautoaraujo414@gmail.com>
2026-03-30 14:30:07 +0000

    fix: reescrever trecho cardapio-dia sem quebra de linha literal

--- src/routes/delivery.routes.js
+++ src/routes/delivery.routes.js
@@ -658,7 +658,11 @@ router.post('/cardapio-hoje/enviar', authDelivery, async (req, res) => {
                 for (const a of assinantes) {
                     try {
                         await EvolutionMultiService.enviarMensagem(instancia._id, a.telefone,
-                            '\xf0\x9f\x8d\xbd\xef\xb8\x8f *Cardápio do Dia!*\n\n' + descricao + '\n\n_Para fazer seu pedido, responda esta mensagem!_'
+                            '🍽️ *Cardápio do Dia!*
+
+' + descricao + '
+
+_Para fazer seu pedido, responda esta mensagem!_'
                         );
                         enviados++;
                     } catch(_) {}
