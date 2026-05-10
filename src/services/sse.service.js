33f7ec326756111578363e5aee0ee9ad32b83020
adautoaraujo414-pixel <adautoaraujo414@gmail.com>
2026-05-10 21:07:48 +0000

    fix: SSE heartbeat 45s, reconexao mais lenta - reduz spam de logs no Render

--- src/services/sse.service.js
+++ src/services/sse.service.js
@@ -17,7 +17,7 @@ const SseService = {
         res.flushHeaders();
         const hb = setInterval(() => {
             try { res.write(': ping\n\n'); } catch(e) { clearInterval(hb); }
-        }, 15000);
+        }, 45000);
         if (!_clientes.has(adminId)) _clientes.set(adminId, new Set());
         _clientes.get(adminId).add(res);
         res.on('close', () => {
@@ -35,7 +35,7 @@ const SseService = {
         res.flushHeaders();
         const hb = setInterval(() => {
             try { res.write(': ping\n\n'); } catch(e) { clearInterval(hb); }
-        }, 15000);
+        }, 45000);
         _entregadores.set(token, res);
         res.on('close', () => {
             clearInterval(hb);
